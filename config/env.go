4c04f985d33aeabfde619ff15ab3bb88843d6ec3
Bhavck <tundepetra0@gmail.com>
2025-12-09 15:12:54 +0100

    new fixes to the config files

--- config/env.go
+++ config/env.go
@@ -22,7 +22,7 @@ func initConfig() Config {
 		PublicHost: getEnv("PUBLIC_HOST", "http://localhost"),
 		Port:       getEnv("PORT", "8080"),
 		DBUser:     getEnv("DB_USER", "root"),
-		DBPassword: getEnv("DB_PASSWORD", "mypassword"),
+		DBPassword: getEnv("DB_PASSWORD", ""),
 		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "localhost"), getEnv("DB_PORT", "3306")),
 		DBName:     getEnv("DB_NAME", "ecommerce"),
 	}
